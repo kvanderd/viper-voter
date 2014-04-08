25801973267c7ab954c7a9be7998b6ff5779e6b4
kvanderd <me@katharine.org>
2014-04-08 08:08:30 -0700

    heroku settings

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
 
 	def index
 		@changes = Change.all
-		@changes = @changes.sort_by { |c| c.aggregate }.last(8).reverse
+		@changes = @changes.sort_by { |c| c.aggregate }.last(9).reverse
 		@category = Category.new
 	end
 
