5a31b3104ba68faa32cadf354841db3409d532c4
kvanderd <me@katharine.org>
2014-06-02 12:02:38 -0700

    hosted gem removed, env vars

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Opinion::Application.config.secret_key_base = '7488243ba4e13aa51902f8e2f8c7350d3b9c0bc9b9d7f10f38633fbf54cbc9df6db8486c6cb7dc1bb647e8f09f2184f1a16f85b745688922bef77a63c08b3012'
+Opinion::Application.config.secret_key_base = ENV["APP_SECRET"]
