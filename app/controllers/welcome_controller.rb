a2433280e1a8b809574f2835d0df7493ab16500d
kvanderd <me@katharine.org>
2014-06-10 16:08:58 -0700

    trending algo to home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
 
 	def index
 		@changes = Change.all
-		@changes = @changes.sort_by { |c| c.aggregate }.last(9).reverse
+		@changes = @changes.sort_by { |c| c.trending_algorithm }.last(9).reverse
 		@categories = Category.all
 	end
 
